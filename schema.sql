205ac1c6017cc8b79571d3bc9c54dd8e937c4e61
Matti <Matti.hamarila@yahoo.com>
2020-07-25 17:18:21 +0300

    DB update

--- schema.sql
+++ schema.sql
@@ -9,3 +9,17 @@ CREATE TABLE messages (
     user_id INTEGER REFERENCES users,
     sent_at TIMESTAMP
 );
+CREATE TABLE labvalues (
+    id SERIAL PRIMARY KEY,
+    lab_name TEXT,
+    user_id INTEGER REFERENCES users,
+    sex TEXT,
+    age INTEGER,
+    diet TEXT,
+    hours_fasted INTEGER,
+    units BOOLEAN,
+    total INTEGER,
+    ldl INTEGER,
+    hdl INTEGER,
+    triglyt INTEGER
+);
