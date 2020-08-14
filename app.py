e4bd3b3fb6a348c784a93a4bc59577d5b6aa5ab1
Matti <Matti.hamarila@yahoo.com>
2020-08-14 13:18:13 +0300

    app.py edited

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from os import getenv
-import routes
-import pandas as pd
 
 
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+import routes
+import pandas as pd
